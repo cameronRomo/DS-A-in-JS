4a08f5f4eb8765d38190536ca7b408d99a3c6c34
Cameron Romo <cromo1225@gmail.com>
2023-06-22 17:28:25 -0600

    Use sliding window pattern to find the max sum of consecutive elements.

---
+++ slidingWindow.js
@@ -0,0 +1,25 @@
+// This pattern involves creating a window which can either be an array or number
+// from one position to another. Depending on a certain condition, the window either
+// increases or closes (and a new window is created) Very useful for keeping track of a subset
+// of data in an array/string etc.
+
+// Write a function called maxSubarraySum which accepts
+// an array of integers and a number called n. The function
+// should calculate the maximum sum of n consecutive elemets in the array.
+
+function maxSubarraySum(arr, num) {
+  let maxSum = 0;
+  let tempSum = 0;
+  if (arr.length < num) return null;
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  console.log("maxSum :>> ", maxSum);
+  return maxSum;
+}
+
+maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3);
